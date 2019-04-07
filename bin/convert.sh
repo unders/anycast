da0aadc07f95e5cf1583669d7e95e6382c455ae4
Anders Törnqvist <anders.tornqvist@gmail.com>
2019-04-07 17:08:08 +0200

    Add tool for handling images to bin folder.

---
+++ bin/convert.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+compress_all() {
+    images=$1
+
+    imageOptim --directory ${images} --quit --verbose
+}
+
+main() {
+    dir=$1
+
+    convert ${dir}/design_crop.jpg -quality 60 -resize 13% ${dir}/design_max.jpg
+    convert ${dir}/design_crop.jpg -quality 60 -resize 13% ${dir}/design_max.webp
+
+    compress_all ${dir}
+}
+
+main src/assets/image
+
+
+# Images
+#
+### Original:
+#   * hosting.jpg - w:h = 2037:1576
+#   * design.jpg  - w:h = 3000:2000
+#   * develop.jpg - w:h = 3000:2002
+#
+# Image Size:
+#            213px:142px
+#            310px:205px
+#            365px:243px
+#            650px:433px (develop)
+#
+### Crop (ratio 1.5):
+#   * hosting_crop.jpg - w:h = 2000:1333
+#   * design_crop.jpg  - w:h = 3000:2000
+#   * develop_crop.jpg - w:h = 3000:2000
+#
+### Max:
+#   * hosting.jpg - w:h =
+#   * design.jpg  - w:h =
+#   * develop.jpg - w:h =
+#
+### Medium:
+#   * hosting.jpg - w:h =
+#   * design.jpg  - w:h =
+#   * develop.jpg - w:h =
+#
+### Min:
+#   * hosting.jpg - w:h =
+#   * design.jpg  - w:h =
+#   * develop.jpg - w:h =
+#
+### Thumb:
+#   * hosting.jpg - w:h = 64:64
+#   * design.jpg  - w:h = 64:64
+#   * develop.jpg - w:h = 64:64
